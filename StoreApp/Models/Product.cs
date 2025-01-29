ba682d50e2f5c1fa12dbc93278b69f7b7edc4be6
southernleo <southernleo7347@gmail.com>
2025-01-29 16:11:45 +0300

    DDD
    Data driven design

---
+++ StoreApp/Models/Product.cs
@@ -0,0 +1,7 @@
+ namespace StoreApp.Models{
+    public class Product{
+        public int ProductId { get; set;}
+        public string ProductName { get; set;}=String.Empty;
+        public decimal Price { get; set;}
+    }
+}
