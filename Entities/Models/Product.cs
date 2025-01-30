d014c0903171402788f8a26af3e61af56e0d69be
southernleo <southernleo7347@gmail.com>
2025-01-30 20:30:10 +0300

    File Operations
    File Operations for imgform finished

--- Entities/Models/Product.cs
+++ Entities/Models/Product.cs
@@ -7,6 +7,8 @@ public class Product
         public int ProductId { get; set;}
         
         public string ProductName { get; set;}=String.Empty;
+        public String? Summary{ get; set;}=String.Empty;
+        public String?ImageUrl{ get; set;}
       
         public decimal Price { get; set;}
         public int?CategoryId{ get; set;}       
