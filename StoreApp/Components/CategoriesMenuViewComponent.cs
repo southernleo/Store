d1bfe1a2e2996ccea5f4cd44429acf36a006e32c
southernleo <southernleo7347@gmail.com>
2025-01-30 11:49:21 +0300

    ViewComponent
    ViewComponents completed

---
+++ StoreApp/Components/CategoriesMenuViewComponent.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using Services.Contracts;
+
+namespace StroreApp.Components{
+    public class CategoriesMenuViewComponent:ViewComponent{
+        private readonly IServiceManager _manager;
+
+        public CategoriesMenuViewComponent(IServiceManager manager){
+            _manager = manager;
+        }
+        
+        public IViewComponentResult Invoke(){
+            var categories=_manager.categoryService.GetAllCategories(false);
+            return View(categories);
+        }
+
+
+    }
+}
