56c5b34d955049727908eff53bdd781aceb15dd9
southernleo <southernleo7347@gmail.com>
2025-02-03 18:54:55 +0300

    User
    User update completed

--- Services/AuthManager.cs
+++ Services/AuthManager.cs
@@ -44,5 +44,42 @@ namespace Services{
         {return  _userManager.Users.ToList(); 
         
          }
+
+        public async Task<IdentityUser> GetOneUser(string userName)
+        {
+          return await _userManager.FindByNameAsync(userName);
+        }
+
+        public async Task<UserDtoForUpdate> GetOneUserForUpdate(string username)
+        {
+             var user=await GetOneUser(username);
+             if(user is not null){
+                var userDto=_mapper.Map<UserDtoForUpdate>(user);
+                userDto.Roles=new HashSet<string>(Roles.Select(r=>r.Name).ToList());
+                userDto.UserRoles=new HashSet<string>(await _userManager.GetRolesAsync(user));
+                return userDto;
+             }
+             throw new Exception("an error occured");
+        }
+
+        public async Task Update(UserDtoForUpdate userDto)
+        { var user=await GetOneUser(userDto.UserName);
+        user.PhoneNumber=userDto.PhoneNumber;
+        user.Email=userDto.Email;
+        if(user is not null){
+            var result=await _userManager.UpdateAsync(user);
+   
+        if(userDto.Roles.Count>0){
+            var userRoles=await _userManager.GetRolesAsync(user);
+            var r1=await _userManager.RemoveFromRolesAsync(user,userRoles);
+            var r2=await _userManager.AddToRolesAsync(user,userDto.Roles);
+
+        }
+        return;
+
+        }
+        throw new Exception("System has problem with user  update");
+        }
+        
     }
 }
--- Services/Contracts/IAuthService.cs
+++ Services/Contracts/IAuthService.cs
@@ -5,6 +5,9 @@ namespace Services.Contracts{
 public interface IAuthService{
     IEnumerable<IdentityRole>Roles { get; }
     IEnumerable<IdentityUser>GetAllUsers();
+    Task<UserDtoForUpdate>GetOneUserForUpdate(string username);
     Task<IdentityResult>CreateUser(UserDtoForCreation userDto);
+    Task<IdentityUser>GetOneUser(string userName);
+    Task Update(UserDtoForUpdate userDto);
 }
 }
